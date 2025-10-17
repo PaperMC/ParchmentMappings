7c146a2a47f83697b9abbfb671f8395cdac6c10f
Lulu13022002 <41980282+Lulu13022002@users.noreply.github.com>
2025-10-17 16:42:47 +0200

    [ci skip] support lambda suggestions and fix array of primitive guess

--- src/enigmaPlugin/kotlin/org/parchmentmc/enigma/EnigmaNameProposalService.kt
+++ src/enigmaPlugin/kotlin/org/parchmentmc/enigma/EnigmaNameProposalService.kt
@@ -25,37 +25,103 @@ class EnigmaNameProposalService() : JarIndexerService, NameProposalService {
         "Lnet/minecraft/client/renderer/SubmitNodeCollection;" to "nodeCollection",
         "Lnet/minecraft/client/renderer/SubmitNodeStorage;" to "nodeStorage",
         "Lnet/minecraft/client/gui/ActiveTextCollector;" to "textCollector",
-        "Lnet/minecraft/world/attribute/SpatialAttributeInterpolator;" to "interpolator",
         "Lnet/minecraft/client/gui/TextAlignment;" to "alignment",
         "Lnet/minecraft/util/FormattedCharSequence;" to "text",
         "Lnet/minecraft/client/gui/navigation/ScreenRectangle;" to "rectangle",
+        $$"Lnet/minecraft/network/chat/FontDescription$AtlasSprite;" to "sprite",
+        "Lnet/minecraft/client/resources/model/MaterialSet;" to "materials",
+        "Lnet/minecraft/client/particle/SpriteSet;" to "sprites",
+        "Lnet/minecraft/client/renderer/texture/TextureAtlasSprite;" to "sprite",
+        "Lnet/minecraft/client/multiplayer/ClientLevel;" to "level",
+        "Lnet/minecraft/client/player/AbstractClientPlayer;" to "player",
+        "Lnet/minecraft/client/input/KeyEvent;" to "event",
+        "Lnet/minecraft/client/input/CharacterEvent;" to "event",
+        "Lnet/minecraft/client/input/MouseButtonEvent;" to "event",
+        "Lnet/minecraft/client/input/MouseButtonInfo;" to "buttonInfo",
+
         // server
         "Lcom/mojang/authlib/GameProfile;" to "profile",
+        "Lnet/minecraft/world/item/component/ResolvableProfile;" to "profile",
+        "Lcom/mojang/brigadier/CommandDispatcher;" to "dispatcher",
+        "Lcom/mojang/brigadier/context/CommandContext;" to "context",
+        "Lnet/minecraft/commands/CommandBuildContext;" to "buildContext",
+        "Lnet/minecraft/commands/CommandSourceStack;" to "source",
+        "Lcom/mojang/brigadier/StringReader;" to "reader",
+        "Lcom/mojang/serialization/DynamicOps;" to "ops",
+        "Lnet/minecraft/resources/RegistryOps;" to "ops",
 
         "Lnet/minecraft/world/item/ItemStack;" to "stack",
         "Lnet/minecraft/world/level/ItemLike;" to "item",
         "Lnet/minecraft/world/level/block/state/BlockState;" to "state",
+
         "Lnet/minecraft/core/BlockPos;" to "pos",
         $$"Lnet/minecraft/core/BlockPos$MutableBlockPos;" to "pos",
+
         "Lnet/minecraft/world/entity/EquipmentSlot;" to "slot",
+        "Lnet/minecraft/resources/ResourceKey;" to "key",
         "Lnet/minecraft/resources/ResourceLocation;" to "location",
         "Lnet/minecraft/world/entity/ContainerUser;" to "user",
         "Lnet/minecraft/world/effect/MobEffectInstance;" to "effectInstance",
+        "Lnet/minecraft/world/attribute/SpatialAttributeInterpolator;" to "interpolator",
+        "Lnet/minecraft/network/chat/FormattedText;" to "text",
+        "Lnet/minecraft/sounds/SoundSource;" to "source",
+        "Lnet/minecraft/nbt/CompoundTag;" to "tag",
+        "Lnet/minecraft/nbt/NbtAccounter;" to "accounter",
+        "Lnet/minecraft/sounds/SoundEvent;" to "sound",
+        "Lnet/minecraft/util/profiling/ProfilerFiller;" to "profiler",
+        "Lnet/minecraft/server/permissions/PermissionSet;" to "permissions",
+        "Lnet/minecraft/world/phys/shapes/CollisionContext;" to "context",
+        "Lnet/minecraft/world/phys/shapes/VoxelShape;" to "shape",
+        "Lnet/minecraft/util/RandomSource;" to "random",
+        "Lnet/minecraft/world/entity/MobCategory;" to "category",
+        "Lnet/minecraft/world/level/block/state/pattern/BlockInWorld;" to "block",
+        "Lnet/minecraft/core/component/DataComponentMap;" to "components",
+        "Lnet/minecraft/core/component/DataComponentType;" to "component",
+        "Lnet/minecraft/core/component/DataComponentGetter;" to "componentGetter",
+        "Lnet/minecraft/world/level/storage/loot/LootContext;" to "context",
+        "Lnet/minecraft/world/attribute/EnvironmentAttribute;" to "attribute",
+        "Lnet/minecraft/world/attribute/EnvironmentAttributeMap;" to "attributes",
+        "Lnet/minecraft/world/attribute/modifier/AttributeModifier;" to "modifier",
+        "Lnet/minecraft/world/level/storage/ValueInput;" to "input",
+        "Lnet/minecraft/world/level/storage/ValueOutput;" to "output",
+        "Lnet/minecraft/world/entity/EntityDimensions;" to "dimensions",
+        "Lnet/minecraft/world/scores/PlayerTeam;" to "team",
+        "Lnet/minecraft/world/InteractionHand;" to "hand",
+        "Lnet/minecraft/CrashReportCategory;" to "category",
+        "Lnet/minecraft/world/entity/EntitySpawnReason;" to "spawnReason",
+
+        "Lnet/minecraft/world/level/chunk/LevelChunk;" to "chunk",
+        "Lnet/minecraft/world/level/chunk/ChunkAccess;" to "chunk",
 
         "Lnet/minecraft/server/level/WorldGenRegion;" to "region",
-        "Lnet/minecraft/server/level/ServerLevel;" to "level",
         "Lnet/minecraft/world/level/LevelReader;" to "level",
+        "Lnet/minecraft/world/level/LevelAccessor;" to "level",
         "Lnet/minecraft/world/level/LevelHeightAccessor;" to "level",
         "Lnet/minecraft/world/level/BlockGetter;" to "level",
+        "Lnet/minecraft/world/level/BlockAndTintGetter;" to "level",
         // "Lnet/minecraft/world/level/CollisionGetter;" to "level",
+        "Lnet/minecraft/server/level/ServerLevel;" to "level",
+        "Lnet/minecraft/world/level/ServerLevelAccessor;" to "level",
 
         "Lnet/minecraft/server/level/ServerPlayer;" to "player",
         "Lnet/minecraft/world/entity/animal/nautilus/AbstractNautilus;" to "nautilus",
         "Lnet/minecraft/world/entity/projectile/AbstractArrow;" to "arrow",
         "Lnet/minecraft/world/entity/monster/AbstractSkeleton;" to "skeleton",
         "Lnet/minecraft/world/entity/animal/horse/AbstractHorse;" to "horse",
+        "Lnet/minecraft/world/entity/player/PlayerSkin;" to "skin",
+        $$"Lnet/minecraft/world/entity/player/PlayerSkin$Patch;" to "skinPatch",
         "Lnet/minecraft/world/entity/HumanoidArm;" to "arm",
         "Lnet/minecraft/util/debug/DebugValueAccess;" to "valueAccess",
+
+        // libraries
+        "Lcom/google/gson/JsonDeserializationContext;" to "context",
+        "Lcom/google/gson/JsonSerializationContext;" to "context",
+        "Lcom/google/gson/JsonObject;" to "json",
+        "Lio/netty/channel/ChannelHandlerContext;" to "ctx",
+        "Lio/netty/channel/ChannelPromise;" to "promise",
+        "Lio/netty/buffer/ByteBuf;" to "buffer",
+        "Lnet/minecraft/network/FriendlyByteBuf;" to "buffer",
+        "Lnet/minecraft/network/RegistryFriendlyByteBuf;" to "buffer",
     )
 
     override fun acceptJar(scope: Set<String>, classProvider: ClassProvider, jarIndex: JarIndex) {
@@ -87,7 +153,7 @@ class EnigmaNameProposalService() : JarIndexerService, NameProposalService {
     override fun proposeName(obfEntry: Entry<*>, remapper: EntryRemapper): Optional<String> {
         if (obfEntry is LocalVariableEntry && obfEntry.isArgument) {
             val parent = obfEntry.parent
-            if (parent != null && !parent.name.startsWith("lambda$")) { // todo handle lambda
+            if (parent != null) {
                 val isStatic = indexer.entryIndex.getMethodAccess(parent)?.isStatic ?: false
 
                 var offsetLvtIndex = 0
@@ -105,13 +171,13 @@ class EnigmaNameProposalService() : JarIndexerService, NameProposalService {
                 }
 
                 val desc = parent.desc.argumentDescs[paramIndex]
-                if (desc.isPrimitive) {
+                if (!desc.containsType()) { // primitive / array of primitive
                     return Optional.empty()
                 }
 
                 val strDesc = desc.toString()
                 var name = suggestions[strDesc] ?: strDesc.substringAfterLast('/').substringAfterLast('$').dropLast(1).replaceFirstChar { it.lowercase() } // relevant type
-                if (paramCanConflict(descStartIndex, parent.desc, desc.typeEntry)) {
+                if (paramCanConflict(descStartIndex, parent.desc, desc.typeEntry)) { // not completely accurate for lambda/inner classes
                     name += (paramIndex + 1)
                 }
                 if (SourceVersion.isKeyword(name)) {
@@ -124,7 +190,7 @@ class EnigmaNameProposalService() : JarIndexerService, NameProposalService {
         return Optional.empty()
     }
 
-    /*
+    /**
      * Transform the given LVT index into a parameter index.
      */
     fun fromLvtToParamIndex(lvtIndex: Int, method: MethodEntry, offsetLvtIndex: Int): Int {
