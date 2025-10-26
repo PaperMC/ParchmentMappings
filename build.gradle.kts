d6dd13989106b4d697f57eca7961be80972521bc
Jason Penilla <11360596+jpenilla@users.noreply.github.com>
2025-10-26 09:47:56 -0700

    Add published version to GH status

--- build.gradle.kts
+++ build.gradle.kts
@@ -247,10 +247,6 @@ publishing {
 val printVersion = tasks.register("printVersion") {
     inputs.property("ver", artifactVersionProvider)
     doFirst {
-        println("Publishing Parchment ${artifactVersionProvider.get()}")
+        println(artifactVersionProvider.get())
     }
-}
-
-tasks.named("publishExportPublicationToPaperRepository") {
-    dependsOn(printVersion)
 }
