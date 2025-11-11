4d8e7a0ca8da8cfb15cde197efebb2190dbb780f
Lulu13022002 <41980282+Lulu13022002@users.noreply.github.com>
2025-11-11 19:00:44 +0100

    25w46a

--- build.gradle.kts
+++ build.gradle.kts
@@ -74,7 +74,7 @@ dependencies {
 }
 
 // For unpick definitions
-val yarnVersion = "25w45a+build.2"
+val yarnVersion = "25w46a+build.2"
 val intermediaryVersion = yarnVersion.substringBefore('+')
 
 dependencies {
