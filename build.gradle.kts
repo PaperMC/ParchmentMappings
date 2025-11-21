031bc6040bdb12fed84413e524191d9cfc5f2e75
Lulu13022002 <41980282+Lulu13022002@users.noreply.github.com>
2025-11-21 19:50:31 +0100

    1.21.11-pre2

--- build.gradle.kts
+++ build.gradle.kts
@@ -74,7 +74,7 @@ dependencies {
 }
 
 // For unpick definitions
-val yarnVersion = "1.21.11-pre1+build.1"
+val yarnVersion = "1.21.11-pre2+build.1"
 val intermediaryVersion = yarnVersion.substringBefore('+')
 
 dependencies {
